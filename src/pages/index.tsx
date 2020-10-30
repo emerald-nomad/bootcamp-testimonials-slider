a33ed0978aed57e08ead6a52f6012b1b6f11080b
Jalen Greene <terrell.greene95@gmail.com>
2020-10-30 07:55:41 -0500

    background images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from 'styles/Home.module.scss'
-
-const testimonials = []
+import {testimonials} from "../data";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
-     
+      <img className={styles['pattern-bg']} src="/images/pattern-bg.svg" alt="Background Pattern" />
+      <img className={styles['pattern-curve']} src="/images/pattern-curve.svg" alt="Curve Pattern"/>
+      <img className={styles['pattern-quotes']} src="/images/pattern-quotes.svg" alt="Quotes Pattern" />
     </div>
   )
 }
