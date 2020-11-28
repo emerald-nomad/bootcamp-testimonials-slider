6c4453fe89b66a5bc0db1ee1ea35e75bb4deb6e2
Jalen Greene <terrell.greene95@gmail.com>
2020-11-27 19:42:55 -0600

    Using external data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import styles from 'styles/Home.module.scss'
 import {testimonials} from "../data";
+import {useState} from "react";
 
 export default function Home() {
+    const [selectedTestimony, setSelectedTestimony] = useState(0);
+
+    const onNextClick = () => {
+        const nextIndex = selectedTestimony + 1;
+
+        if (nextIndex === testimonials.length) {
+            setSelectedTestimony(0);
+        } else {
+            setSelectedTestimony(selectedTestimony + 1);
+        }
+    }
+
+    const onPrevClick = () => {
+        const nextIndex = selectedTestimony - 1;
+
+        if (nextIndex === -1) {
+            setSelectedTestimony(testimonials.length - 1);
+        } else {
+            setSelectedTestimony(selectedTestimony - 1);
+        }
+    }
+
     return (
         <div className={styles.container}>
             <img className={styles['pattern-bg']} src="/images/pattern-bg.svg" alt="Background Pattern"/>
@@ -9,24 +32,24 @@ export default function Home() {
             <img className={styles['pattern-quotes']} src="/images/pattern-quotes.svg" alt="Quotes Pattern"/>
 
             <div className={styles.selector}>
-                <img src="/images/image-tanya.jpg" alt="Author Image" className={styles['selector__img']}/>
+                <img src={testimonials[selectedTestimony].imgPath} alt="Author Image"
+                     className={styles['selector__img']}/>
                 <div className={styles['selector__btn-container']}>
                     <button className={styles['selector__btn']}>
-                        <img src="/images/icon-prev.svg" alt="Previous Icon"/>
+                        <img src="/images/icon-prev.svg" alt="Previous Icon" onClick={onPrevClick}/>
                     </button>
                     <button className={styles['selector__btn']}>
-                        <img src="/images/icon-next.svg" alt="Next Icon"/>
+                        <img src="/images/icon-next.svg" alt="Next Icon" onClick={onNextClick}/>
                     </button>
                 </div>
             </div>
 
             <div className={styles.testimony}>
                 <p className={styles['testimony__content']}>
-                    " I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t
-                    recommend this course enough. I’m now in the job of my dreams and so excited about the future. "
+                    " {testimonials[selectedTestimony].quote} "
                 </p>
-                <p className={styles['testimony__author']}>Tanya Sinclair</p>
-                <p className={styles['testimony__position']}>UX Engineer</p>
+                <p className={styles['testimony__author']}>{testimonials[selectedTestimony].name}</p>
+                <p className={styles['testimony__position']}>{testimonials[selectedTestimony].position}</p>
 
             </div>
         </div>
